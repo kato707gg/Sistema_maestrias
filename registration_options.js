aed1a95225f8b4a2696a083e33b0fb4c39118e6c
kato707gg <katoh707@gmail.com>
2024-04-19 23:33:32 -0600

    Registro con diseño

--- registration_options.js
+++ registration_options.js
@@ -14,23 +14,6 @@ function showInitialContent() {
     registrationOptions.style.display = "none"; // Oculta el formulario de registro
 }
 
-
-var inputs = document.querySelectorAll('input[type="text"], input[type="number"], input[type="tel"], input[type="email"]');
-// Agrega eventos focus y blur a cada campo de entrada
-inputs.forEach(function(input) {
-    input.addEventListener('focus', function() {
-        // Al hacer clic en el campo de entrada, oculta la etiqueta
-        input.previousElementSibling.classList.add('input-focus');
-    });
-
-    input.addEventListener('blur', function() {
-        // Verifica si el campo está vacío al salir del foco
-        if (input.value === '') {
-            input.previousElementSibling.classList.remove('input-focus');
-        }
-    });
-});
-
 function validarTelefono(event) {
     event.target.value = event.target.value.replace(/\D/g, '').slice(0, 10);
 }
